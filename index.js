f9cad1fb27f410d07af7a0e90e3692fdfe0dae22
daedalus1948 <daedalus1948@gmail.com>
2022-11-23 16:20:58 +0100

    added docker

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+'use strict';
 
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
